a0b28eebd3243b4da8bef49ec998d214fef396ba
domiszy610 <domiszy610@student.polsl.pl>
2022-02-04 22:36:37 +0100

    Adding created maze to Game Object

--- Assets/Scripts/MazeGenerator/MazeRenderer.cs
+++ Assets/Scripts/MazeGenerator/MazeRenderer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class MazeRenderer : MonoBehaviour
 {
-        [SerializeField]
-    [Range(1, 50)]
-    private int width = 10;
+    [SerializeField]
+    [Range(1, 20)]
+    private int width = 30;
 
     [SerializeField]
-    [Range(1, 50)]
-    private int height = 10;
+    [Range(1, 20)]
+    private int height = 30;
 
     [SerializeField]
     private float size = 1f;
@@ -22,48 +22,64 @@ public class MazeRenderer : MonoBehaviour
     [SerializeField]
     private Transform floorPrefab = null;
     
+    
+    private GameObject rightWalls;
+    private GameObject leftWalls;
+    private GameObject bottomWalls;
+    private GameObject topWalls;
+    
     void Start()
     {
         var maze = MazeGenerator.Generate(width, height);
-        Draw(maze);
+        GameObject mazeObject = Draw(maze);
     }
 
-    private void Draw(WallState[,] maze)
+    private GameObject Draw(WallState[,] maze)
     {
+        GameObject mazeObject = new GameObject("Maze");
+        Transform floor = Instantiate(floorPrefab, transform);
 
-        var floor = Instantiate(floorPrefab, transform);
-        floor.localScale = new Vector3(width, 1, height);
+        float widthFloor = width + width*0.01f;
+        float heightFloor = height + height*0.01f;
+        floor.localScale = new Vector3(widthFloor, 0.2f, heightFloor);
+        floor.position += new Vector3(-0.5f, -0.5f, -0.5f);
 
         for (int i = 0; i < width; ++i)
         {
             for (int j = 0; j < height; ++j)
             {
-                var cell = maze[i, j];
-                var position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
+                WallState cell = maze[i, j];
+                Vector3 position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
 
                 if (cell.HasFlag(WallState.UP))
                 {
-                    var topWall = Instantiate(wallPrefab, transform) as Transform;
+                    Transform topWall = Instantiate(wallPrefab, transform);
                     topWall.position = position + new Vector3(0, 0, size / 2);
                     topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
+                    topWalls = topWall.gameObject;
+                    SetParentObject(mazeObject, topWalls);
                 }
 
                 if (cell.HasFlag(WallState.LEFT))
                 {
-                    var leftWall = Instantiate(wallPrefab, transform) as Transform;
+                    Transform leftWall = Instantiate(wallPrefab, transform);
                     leftWall.position = position + new Vector3(-size / 2, 0, 0);
                     leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
                     leftWall.eulerAngles = new Vector3(0, 90, 0);
+                    leftWalls = leftWall.gameObject;
+                    SetParentObject(mazeObject, leftWalls);
                 }
 
                 if (i == width - 1)
                 {
                     if (cell.HasFlag(WallState.RIGHT))
                     {
-                        var rightWall = Instantiate(wallPrefab, transform) as Transform;
+                        Transform rightWall = Instantiate(wallPrefab, transform);
                         rightWall.position = position + new Vector3(+size / 2, 0, 0);
                         rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
                         rightWall.eulerAngles = new Vector3(0, 90, 0);
+                        rightWalls = rightWall.gameObject;
+                        SetParentObject(mazeObject, rightWalls);
                     }
                 }
 
@@ -71,14 +87,30 @@ public class MazeRenderer : MonoBehaviour
                 {
                     if (cell.HasFlag(WallState.DOWN))
                     {
-                        var bottomWall = Instantiate(wallPrefab, transform) as Transform;
+                        Transform bottomWall = Instantiate(wallPrefab, transform);
                         bottomWall.position = position + new Vector3(0, 0, -size / 2);
                         bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
+                        bottomWalls = bottomWall.gameObject;
+                        SetParentObject(mazeObject, bottomWalls);
                     }
                 }
+                
             }
 
-        }
 
+        }
+        
+  
+        GameObject floorObj = floor.gameObject;
+        SetParentObject(mazeObject, floorObj);
+
+        return mazeObject;
+        
     }
+    
+    public void SetParentObject(GameObject parentOb, GameObject childObject)
+    {
+        childObject.transform.SetParent(parentOb.transform);
+    }
+
 }
