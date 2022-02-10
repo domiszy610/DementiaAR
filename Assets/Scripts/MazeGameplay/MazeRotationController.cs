6cfd3bc2131843f702cf781f8b1de9743cb6dbed
domiszy610 <domiszy610@student.polsl.pl>
2022-02-10 21:43:34 +0100

    Fixed Maze Rotation Script, Changes in MazeLevel Scene

--- Assets/Scripts/MazeGameplay/MazeRotationController.cs
+++ Assets/Scripts/MazeGameplay/MazeRotationController.cs
@@ -4,20 +4,36 @@ namespace MazeGameplay
 {
     public class MazeRotationController : MonoBehaviour
     {
+        [SerializeField]
+        private float rotationPower;
+        [SerializeField]
+        private float rotationSpeed;
+
+        private float angleY = 0.0f;
+        private float angleX = 0.0f;
+        private Touch screenTouch;
+        private Vector2 input;
         private void Update()
         {
             if (Input.touchCount > 0)
             {
+                screenTouch = Input.GetTouch(0);
+                input = screenTouch.deltaPosition;
                 RotateMaze();
             }
         
         }
-
-        private void RotateMaze()
+        
+        void RotateMaze()
         {
-            Touch screenTouch = Input.GetTouch(0);
-            transform.Rotate(screenTouch.deltaPosition.y, screenTouch.deltaPosition.x, 0f);
-
+            angleY += input.x * rotationPower*Time.deltaTime;
+            angleY = Mathf.Clamp(angleY, -90.0f, 90.0f);
+            
+            angleX += input.y * rotationPower*Time.deltaTime;
+            angleX = Mathf.Clamp(angleX, -45.0f, 45.0f);
+            
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(-angleX, -angleY, 0.0f), Time.deltaTime*rotationSpeed);
         }
+        
     }
 }
