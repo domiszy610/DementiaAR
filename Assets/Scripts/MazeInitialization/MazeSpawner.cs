b32bb73b3744da55611ebce2c999ee004232ee49
domiszy610 <domiszy610@student.polsl.pl>
2022-02-10 02:11:45 +0100

    Fix in MazeSpawner and MazeLevel Scene

--- Assets/Scripts/MazeInitialization/MazeSpawner.cs
+++ Assets/Scripts/MazeInitialization/MazeSpawner.cs
@@ -1,54 +1,68 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 namespace MazeInitialization
 {
-    [RequireComponent((typeof(ARPlaneManager)))]
+    [RequireComponent(typeof(ARRaycastManager))]
     public class MazeSpawner : MonoBehaviour
     {
+        #region Serialized Fields
+
         [SerializeField]
         private GameObject mazePrefab;
-        // [SerializeField]
-        // private GameObject spawnedSphere;
-
         [SerializeField]
         private GameObject instructionPanel;
         [SerializeField]
         private Button dismissButton;
-
-       
-        private GameObject placeablePrefab;
-
-
-
         [SerializeField]
-        private ARPlaneManager arPlaneManager;
-        
-        
+        private Camera arCamera;
+
+        #endregion
+
+        #region Private Fields
+
+        private Vector3 mazePosition = new Vector3(0.5f, 0.5f, 0f);
+
+        private ARRaycastManager arRaycastManager;
+
+        private static List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
+
+        #endregion
+
+        #region Unity Callbacks
 
         private void Awake()
         {
+            arRaycastManager = GetComponent<ARRaycastManager>();
             dismissButton.onClick.AddListener(Dismiss);
-            arPlaneManager = GetComponent<ARPlaneManager>();
-
-            arPlaneManager.planesChanged += PlaneChanged;
         }
+
+        private void Update()
+        {
+            if (mazePrefab)
+            {
+                if (arRaycastManager.Raycast(arCamera.ViewportPointToRay(mazePosition), raycastHits, TrackableType.PlaneWithinPolygon))
+                {
+                    MoveMazeObject();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
 
         private void Dismiss() => instructionPanel.SetActive(false);
 
-        private void PlaneChanged(ARPlanesChangedEventArgs args)
+        private void MoveMazeObject()
         {
-            if (args.added != null && placeablePrefab == null)
-            {
-                ARPlane arPlane = args.added[0];
-                placeablePrefab = Instantiate(mazePrefab, arPlane.transform.position, Quaternion.identity); //Move Instantiate to Awake, here change position
-                
-            }
+            Pose hitPose = raycastHits[0].pose;
+            mazePrefab.transform.position = hitPose.position;
         }
- 
+
+        #endregion
     }
 }
