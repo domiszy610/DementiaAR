9a7c56f0f06f10b736e4209bb3c23a4e1ef40d02
domiszy610 <domiszy610@student.polsl.pl>
2022-05-02 23:51:10 +0200

    Added LevelInfoController

---
+++ Assets/Scripts/UI/LevelInfoController.cs
@@ -0,0 +1,42 @@
+using System;
+using ScriptableObjects;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelInfoController : MonoBehaviour
+{
+        [SerializeField]
+        private SceneLoaderContainer sceneLoaderContainer;
+        [SerializeField]
+        private TextMeshProUGUI levelInfoText;
+
+        private int currentIndex;
+
+        private void Awake()
+        {
+                currentIndex = GetCurrentLevelIndex();
+                ChangeLevelText(currentIndex);
+                sceneLoaderContainer.OnModifiedCurrentLevel += NextLevel;
+        }
+
+        private void OnDisable()
+        {
+                sceneLoaderContainer.OnModifiedCurrentLevel -= NextLevel;
+        }
+
+        private void NextLevel()
+        {
+                currentIndex = GetCurrentLevelIndex();
+                ChangeLevelText(currentIndex);
+        }
+
+        private void ChangeLevelText(int currentLevelIndex)
+        {
+                int level = currentLevelIndex + 1;
+                levelInfoText.text = "POZIOM " + level.ToString();
+        }
+
+
+        private int GetCurrentLevelIndex() => sceneLoaderContainer.CurrentLevelIndex;
+}
