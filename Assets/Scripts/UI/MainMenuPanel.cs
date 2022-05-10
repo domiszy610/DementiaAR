e1ccd99927cc83e6e9d90731000b7c85eb2406a8
domiszy610 <domiszy610@student.polsl.pl>
2022-05-10 21:01:25 +0200

    Added Instruction Button Calback

--- Assets/Scripts/UI/MainMenuPanel.cs
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -29,6 +29,11 @@ public class MainMenuPanel : MonoBehaviour
     {
         SceneManager.LoadScene(sceneLoaderContainer.MazeLevelSelectionPath);
     }
+    
+    public void ReadInstruction()
+    {
+        SceneManager.LoadScene(sceneLoaderContainer.InstructionPath);
+    }
 
     public void QuitGame()
     {
